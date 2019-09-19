58fcdc8ce35a727a8eac4e8cb2152ec248012c23
Matt Bierner <matb@microsoft.com>
2019-09-18 17:21:16 -0700

    Use vscode-test instead of vscode

--- test/index.js
+++ test/index.js
@@ -1,28 +1,31 @@
-/*---------------------------------------------------------------------------------------------
- *  Copyright (c) Microsoft Corporation. All rights reserved.
- *  Licensed under the MIT License. See License.txt in the project root for license information.
- *--------------------------------------------------------------------------------------------*/
+const path = require('path');
+const Mocha = require('mocha');
+const glob = require('glob');
 
-//
-// PLEASE DO NOT MODIFY / DELETE UNLESS YOU KNOW WHAT YOU ARE DOING
-//
-// This file is providing the test runner to use when running extension tests.
-// By default the test runner in use is Mocha based.
-//
-// You can provide your own test runner if you want to override it by exporting
-// a function run(testRoot: string, clb: (error:Error) => void) that the extension
-// host can call to run the tests. The test runner is expected to use console.log
-// to report the results back to the caller. When the tests are finished, return
-// a possible error to the callback or null if none.
+module.exports.run = (testsRoot, cb) => {
+	// Create the mocha test
+	const mocha = new Mocha({
+		ui: 'tdd',
+	});
+	mocha.useColors(true);
 
-const testRunner = require('vscode/lib/testrunner');
+	glob('**/**.test.js', { cwd: testsRoot }, (err, files) => {
+		if (err) {
+			return cb(err);
+		}
 
-// You can directly control Mocha options by uncommenting the following lines
-// See https://github.com/mochajs/mocha/wiki/Using-mocha-programmatically#set-options for more info
-testRunner.configure({
-    ui: 'tdd', 		// the TDD UI is being used in extension.test.ts (suite, test, etc.)
-    useColors: process.platform !== 'win32', // colored output from test results (only windows cannot handle)
-    timeout: 60000
-});
+		// Add files to the test suite
+		files.forEach(f => mocha.addFile(path.resolve(testsRoot, f)));
 
-module.exports = testRunner;
+		try {
+			// Run the mocha test
+			mocha
+				.run(failures => {
+					cb(null, failures);
+				});
+
+		} catch (err) {
+			cb(err);
+		}
+	});
+}
